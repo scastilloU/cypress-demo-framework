ce8322b7849ad213f6875d50ac3d1a9ea97b58ed
scastillo <sergio.castillo@ucreativa.com>
2023-10-20 18:34:23 -0600

    Asd

---
+++ cypress/integration/myExample.cy.js
@@ -0,0 +1,10 @@
+// This is a basic Cypress test script
+describe('Cypress Website Test', () => {
+    it('should visit the Cypress website', () => {
+      // Visit the Cypress website
+      cy.visit('https://www.cypress.io/');
+  
+      // Check for the presence of the "Get Started" button
+      cy.contains('Get Started').should('be.visible');
+    });
+  });
